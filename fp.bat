522272f311ab64e121c7c2b67be2794e7cb3ec88
Matt Stevens <matt@mattstevens.co.uk>
2023-07-14 00:02:17 +0100

    fp.bat: use vswhere to find vcvarsall.bat

--- fp.bat
+++ fp.bat
@@ -1,6 +1,23 @@
 cd valve
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+for /f "usebackq tokens=*" %%a in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+   set "VSINSTALLPATH=%%a"
+)
+
+if not defined VSINSTALLPATH (
+   echo No Visual Studio installation detected.
+   goto end
+)
+
+if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
+   call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64
+) else (
+   goto end
+)
+
 call fp_config.bat
 jom
 jom install
+
+:end
 pause
