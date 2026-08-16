ee42bbf028668084f4f4d0ec4c41a7c81ee7c880
Lorenz Junglas <4759511+lolleko@users.noreply.github.com>
2026-08-16 22:09:45 +0200

    Implement Qt memoverride for POSIX

--- valve/qtbase/src/valve_memoverride/valve_memoverride.cpp
+++ valve/qtbase/src/valve_memoverride/valve_memoverride.cpp
@@ -1,7 +1,10 @@
 #ifdef _WIN32
 #include <Windows.h>
 #else
-#error Need to implement memoverride for Qt on non-win32 platforms or decide it's not needed
+// s&box: POSIX port. dlsym on RTLD_DEFAULT searches every object already loaded into the
+// process, which is where tier0 will be by the time Qt initialises - the equivalent of
+// GetModuleHandle + GetProcAddress on Windows.
+#include <dlfcn.h>
 #endif // _WIN32
 
 #include <cstdlib>
@@ -10,8 +13,6 @@
 // Disable warning: initializers put in library initialization area
 #pragma warning(disable:4073)
 #pragma init_seg(lib)
-#else
-#pragma error( "Error: static object initialization order is not guaranteed!" );
 #endif
 
 typedef void *( *QAlloc_t )( size_t );
@@ -26,8 +27,12 @@ typedef void ( *QGetCustomAllocators_t)( QAlloc_t *pAlloc, QFree_t *pFree, QReal
 
 static int install_custom_allocators()
 {
+#ifdef _WIN32
 	HMODULE appModule = GetModuleHandleA( "tier0.dll" );
 	QGetCustomAllocators_t pGetCustomAllocators = (QGetCustomAllocators_t)GetProcAddress( appModule, "Qt_GetCustomAllocators" );
+#else
+	QGetCustomAllocators_t pGetCustomAllocators = (QGetCustomAllocators_t)dlsym( RTLD_DEFAULT, "Qt_GetCustomAllocators" );
+#endif
 	if ( pGetCustomAllocators )
 	{
 		pGetCustomAllocators( &s_pAlloc, &s_pFree, &s_pRealloc );
@@ -36,7 +41,18 @@ static int install_custom_allocators()
 	return 1;
 }
 
+#ifdef _WIN32
+// init_seg(lib) above orders this ahead of ordinary static initialisers.
 static int qt_force_install_custom_allocators = install_custom_allocators();
+#else
+// The GCC equivalent of init_seg(lib): a low priority number runs early, before any
+// static initialiser that might allocate.
+__attribute__( ( constructor( 101 ) ) )
+static void qt_force_install_custom_allocators()
+{
+	install_custom_allocators();
+}
+#endif
 
 void *operator new[]( size_t size )
 {
